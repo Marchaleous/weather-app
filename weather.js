38302795c83291fa67807aa227e0d9da6858a2d7
Marchaleous <rmarchal4@gmail.com>
2020-08-15 00:54:57 -0500

    removed api key

--- weather.js
+++ weather.js
@@ -1,10 +1,9 @@
 // API INFO
-// API KEY: 90d2a7f29a179c46eaebe820dd8678dd
 // https://api.openweathermap.org/data/2.5/weather?q={CITY NAME HERE},{STATE CODE HERE}&appid={API KEY HERE}&units={default(K), metric(C), OR imperial(F)}
 
 class Weather {
   constructor(city, state) {
-    this.apiKey = '90d2a7f29a179c46eaebe820dd8678dd';
+    this.apiKey = '';
     this.city = city;
     this.state = state;
     // this.units = units;
